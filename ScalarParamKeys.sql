6e52d6280e41b68b0165aff7f03e5275f7b36145
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-24 22:44:24 +0530

    Add files via upload

---
+++ ScalarParamKeys.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS dbo."ScalarParamKeys"
+(
+    "ScalarParamKeyId"          SMALLINT                                  NOT NULL,
+    "ScalarKey"                 VARCHAR(25)  COLLATE dbo.case_insensitive NOT NULL,
+    "CustomerServiceId"         INTEGER                                   NOT NULL,
+    "Description"               VARCHAR(255) COLLATE dbo.case_insensitive NULL,
+    "DateCreated"               TIMESTAMP                                 NOT NULL,
+    "DateAmended"               TIMESTAMP                                 NOT NULL,
+    "WhoAmended_nt_username"    VARCHAR(255) COLLATE dbo.case_insensitive NOT NULL,
+    "WhoAmended_hostname"       VARCHAR(255) COLLATE dbo.case_insensitive NOT NULL,
+    "AuditId"                   UUID                                      NULL,
+    "AuditActionType"           CHAR(2)      COLLATE dbo.case_insensitive NOT NULL,
+    "AuditSequenceNo"           INTEGER      GENERATED ALWAYS AS IDENTITY NOT NULL ,
+    "LoginName"                 VARCHAR(256) COLLATE dbo.case_insensitive NULL,
+    "HostName"                  VARCHAR(256) COLLATE dbo.case_insensitive NULL,
+    "ActionTime"                TIMESTAMP                                 NULL,
+    CONSTRAINT "PK_ScalarParamKeys_CU01" PRIMARY KEY  
+    (
+			
+            "AuditSequenceNo"
+    )		
+);
+
+
+GRANT INSERT ON dbo."ScalarParamKeys" TO PUBLIC;
